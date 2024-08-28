d3735f7d2d59388c17150b7a9ae612c30fa4e524
geckro <94733731+geckro@users.noreply.github.com>
2024-08-28 15:32:14 +0800

    feat: remove all terraria tips and add custom Argos tips

---
+++ Common/RemoveTips.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using Terraria.ModLoader;
+
+namespace Argos.Common;
+
+public class RemoveTips : ModSystem
+{
+    public override void ModifyGameTipVisibility(IReadOnlyList<GameTipData> gameTips) {
+        foreach (GameTipData tip in gameTips)
+        {
+            if (tip.FullName.Contains("Terraria/"))
+            {
+                tip.Hide();
+            }
+        }
+    }
+}
