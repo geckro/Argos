91467bc9b565586c76d2c2b5a099b6dd66546269
geckro <94733731+geckro@users.noreply.github.com>
2024-08-28 16:19:35 +0800

    feat: change default values of some items

---
+++ Common/GlobalItems/ChangeItems.cs
@@ -0,0 +1,46 @@
+﻿using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace Argos.Common.GlobalItems;
+
+public class CactusPickaxe : GlobalItem
+{
+    public override bool AppliesToEntity(Item item, bool lateInstantiation) {
+        return item.type == ItemID.CactusPickaxe;
+    }
+
+    public override void SetDefaults(Item item) {
+        item.StatsModifiedBy.Add(Mod);
+        item.tileBoost = -1;
+    }
+}
+
+public class CopperPickaxe : GlobalItem
+{
+    public override bool AppliesToEntity(Item item, bool lateInstantiation) {
+        return item.type == ItemID.CopperPickaxe;
+    }
+
+    public override void SetDefaults(Item item) {
+        item.StatsModifiedBy.Add(Mod);
+        item.tileBoost = 0;
+    }
+}
+
+public class Zenith : GlobalItem
+{
+    public override bool AppliesToEntity(Item item, bool lateInstantiation) {
+        return item.type == ItemID.Zenith;
+    }
+
+    public override void SetDefaults(Item item)
+    {
+        if (EnabledMods.AnyContentModEnabled)
+        {
+            return;
+        }
+
+        item.useTime = 1;  // is originally 30, :troll:
+    }
+}
