014e0f2f0cf8fc41d76ceac23e20c9c5a33f53ed
geckro <94733731+geckro@users.noreply.github.com>
2024-08-28 13:32:40 +0800

    fix: remove journey mode starting inventory aswell

--- Common/PlayerInventory.cs
+++ Common/PlayerInventory.cs
@@ -9,8 +9,21 @@ public class PlayerInventory : ModPlayer
 {
     private readonly (string mod, short itemId)[] _inventoryItemsToRemove =
     [
+            // Classic/Expert/Master mode items
             ("Terraria", ItemID.CopperShortsword),
-            ("Terraria", ItemID.CopperPickaxe)
+            ("Terraria", ItemID.CopperPickaxe),
+
+            // Journey mode items
+            ("Terraria", ItemID.BabyBirdStaff),
+            // ("Terraria", ItemID.CreativeWings),  // TODO: Does not get removed
+            ("Terraria", ItemID.GrapplingHook),
+            ("Terraria", ItemID.IronHammer),
+            ("Terraria", ItemID.IronPickaxe),
+            ("Terraria", ItemID.IronShortsword),
+            ("Terraria", ItemID.MagicMirror),
+            ("Terraria", ItemID.Rope),
+            ("Terraria", ItemID.Torch),
+            ("Terraria", ItemID.WolfMountItem),
     ];
 
     public override void ModifyStartingInventory(IReadOnlyDictionary<string, List<Item>> items, bool mCoreDeath)
