7fc100e402ccda9978ca47c9fa31a20364ed86b0
geckro <94733731+geckro@users.noreply.github.com>
2024-08-28 13:25:22 +0800

    feat: remove copper shortsword and pickaxe from the players inventory

---
+++ Common/PlayerInventory.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace Argos.Common;
+
+public class PlayerInventory : ModPlayer
+{
+    private readonly (string mod, short itemId)[] _inventoryItemsToRemove =
+    [
+            ("Terraria", ItemID.CopperShortsword),
+            ("Terraria", ItemID.CopperPickaxe)
+    ];
+
+    public override void ModifyStartingInventory(IReadOnlyDictionary<string, List<Item>> items, bool mCoreDeath)
+    {
+        foreach ((string mod, short itemId) inventoryItem in _inventoryItemsToRemove)
+        {
+            items[inventoryItem.mod].RemoveAll(item => item.type == inventoryItem.itemId);
+        }
+    }
+}
